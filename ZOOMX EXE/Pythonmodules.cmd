f6b2fe5a632fd66f4233d0f0fbb01b1723b4ea33
NitinsaiAvirneni <82219591+NitinsaiAvirneni@users.noreply.github.com>
2021-09-09 17:06:25 +0530

    files

---
+++ ZOOMX EXE/Pythonmodules.cmd
@@ -0,0 +1,21 @@
+python -m pip install --user  --upgrade pip
+pip install subprocess
+pip install pynput
+pip install webbrowser
+pip install selenium
+pip install webdriver
+pip install Controller
+pip install requests
+pip install hashlib
+pip install bcrypt
+pip install tkinter
+pip install SpeechRecognition
+pip install pyaudio
+pip install webdriver_manager
+pip install numpy
+pip install pyautogui
+pip install opencv-python
+pip install Pillow
+pip install pyaudio
+pip install screen-recorder-python
+pip install tkcalendar
